45afa1226cd284de4fda894112f7330c85c34d64
Melis Nur KILIÇ <melisnurklc0@gmail.com>
2023-10-23 22:07:09 +0300

    Storage Repository

---
+++ app/src/main/java/com/example/notes/repository/StorageRepository.kt
@@ -0,0 +1,118 @@
+package com.example.notes.repository
+
+import com.example.notes.models.Notes
+import com.google.firebase.Timestamp
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.ListenerRegistration
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.toObject
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+
+const val NOTES_COLLECTION_REF = "notes"
+
+class StorageRepository() {
+
+    val user = Firebase.auth.currentUser
+    fun hasUser():Boolean = Firebase.auth.currentUser != null
+
+    fun getUserId():String = Firebase.auth.currentUser?.uid.orEmpty()
+
+    private val notesRef:CollectionReference = Firebase.firestore.collection(NOTES_COLLECTION_REF)
+
+    fun getUserNotes(
+        userId:String
+    ):Flow<Resources<List<Notes>>> = callbackFlow {
+        var snapshotStateListener:ListenerRegistration? = null
+
+        try {
+            snapshotStateListener = notesRef.orderBy("timestamp")
+                .whereEqualTo("userId", userId).addSnapshotListener { value, error ->
+                    val response = if(value != null){
+                        val notes = value.toObjects(Notes::class.java)
+                        Resources.success(data = notes)
+                    }else{
+                        Resources.error(throwable = error?.cause)
+                    }
+                    trySend(response)
+                }
+
+        }catch (e:Exception){
+            trySend(Resources.error(e?.cause))
+            e.printStackTrace()
+        }
+
+        awaitClose {
+            snapshotStateListener?.remove()
+        }
+    }
+
+    fun getNote(
+        noteId:String,
+        onError:(Throwable?) -> Unit,
+        onSuccess: (Notes?) -> Unit
+    ){
+        notesRef.document(noteId).get().addOnSuccessListener {
+            onSuccess.invoke(it?.toObject(Notes::class.java))
+        }.addOnFailureListener { result ->
+            onError.invoke(result)
+        }
+    }
+
+    fun addNote(
+        userId:String,
+        title:String,
+        description:String,
+        timestamp:Timestamp,
+        color:Int = 0,
+        onComplete:(Boolean) -> Unit
+    ){
+        val documentId = notesRef.document().id
+        val note = Notes(userId,title,description,timestamp,color,documentId)
+
+        notesRef.document(documentId).set(note).addOnCompleteListener { result ->
+            onComplete.invoke(result.isSuccessful)
+        }
+    }
+
+    fun deleteNote(
+        noteId: String, onComplete: (Boolean) -> Unit
+    ){
+        notesRef.document(noteId).delete().addOnCompleteListener {
+            onComplete.invoke(it.isSuccessful)
+        }
+    }
+
+    fun updateNote(
+        noteId:String,
+        title:String,
+        description:String,
+        color: Int,
+        onResult: (Boolean) -> Unit
+    ){
+        val updateData = hashMapOf<String,Any>(
+            "colorIndex" to color,
+            "description" to description,
+            "title" to title
+        )
+
+        notesRef.document(noteId).update(updateData).addOnCompleteListener {
+            onResult.invoke(it.isSuccessful)
+        }
+    }
+
+
+
+}
+
+sealed class Resources<T>(
+    val data: T? = null,
+    val throwable: Throwable?? = null,
+){
+    class loading<T>:Resources<T>()
+    class success<T>(data: T?):Resources<T>(data= data)
+    class error<T>(throwable: Throwable?):Resources<T>(throwable = throwable)
+}
